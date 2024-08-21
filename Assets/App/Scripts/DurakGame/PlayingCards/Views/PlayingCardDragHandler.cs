9418e63c7f792d54592531f3c98da50149e2aeb1
Aleksandr Krotov <73738250+PhlegmaticOne@users.noreply.github.com>
2024-08-21 21:14:39 +0300

    Setup packages for project

---
+++ Assets/App/Scripts/DurakGame/PlayingCards/Views/PlayingCardDragHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace App.Scripts.DurakGame.PlayingCards.Views
+{
+    public class PlayingCardDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Debug.Log("Begin drag");
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = eventData.pointerCurrentRaycast.worldPosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("End drag");
+        }
+    }
+}
